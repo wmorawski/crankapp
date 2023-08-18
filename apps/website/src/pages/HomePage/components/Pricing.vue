b47fb824f36052b06efb2d10def05f42c1e355f2
Wiktor Morawski <wiktor.morawski@recruitee.com>
2023-08-18 22:34:09 +0200

    feat: add pricing component

---
+++ apps/website/src/pages/HomePage/components/Pricing.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-4xl text-center">
+        <h2 class="text-base font-semibold leading-7 text-indigo-600">Pricing</h2>
+        <p class="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">Simple subscription idea</p>
+      </div>
+      <p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">Use for free or get more for little price</p>
+      <div class="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div v-for="(tier, tierIdx) in tiers" :key="tier.id" :class="[tier.paid ? 'lg:z-10 lg:rounded-bl-none' : 'lg:mt-8', tierIdx === 0 ? 'lg:rounded-r-none' : '', 'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10']">
+          <div>
+            <div class="flex items-center justify-between gap-x-4">
+              <h3 :id="tier.id" :class="[tier.paid ? 'text-indigo-600' : 'text-gray-900', 'text-lg font-semibold leading-8']">{{ tier.name }}</h3>
+            </div>
+            <p class="mt-4 text-sm leading-6 text-gray-600">{{ tier.description }}</p>
+            <p class="mt-6 flex items-baseline gap-x-1">
+              <span class="text-4xl font-bold tracking-tight text-gray-900">{{ tier.priceMonthly }}</span>
+              <span v-if="tier.paid" class="text-sm font-semibold leading-6 text-gray-600">/month</span>
+            </p>
+            <ul role="list" class="mt-8 space-y-3 text-sm leading-6 text-gray-600">
+              <li v-for="feature in tier.features" :key="feature" class="flex gap-x-3">
+                <CheckIcon class="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                {{ feature }}
+              </li>
+            </ul>
+          </div>
+          <a :href="tier.href" :aria-describedby="tier.id" :class="[tier.paid ? 'bg-indigo-600 text-white shadow-sm hover:bg-indigo-500' : 'text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300', 'mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600']">{{tier.cta}}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { CheckIcon } from '@heroicons/vue/20/solid'
+
+const tiers = [
+  {
+    name: 'Starter',
+    id: 'tier-starter',
+    href: '#',
+    cta: 'Get started',
+    priceMonthly: 'Free',
+    description: 'The essentials to provide your best work for clients.',
+    features: ['5 products', 'Up to 1,000 subscribers', 'Basic analytics', '48-hour support response time'],
+    paid: false,
+  },
+  {
+    name: 'Pro',
+    id: 'tier-pro',
+    href: '#',
+    cta: 'Subscribe',
+    priceMonthly: '$3',
+    description: 'A plan that scales with your rapidly growing business.',
+    features: [
+      'Everything what in free',
+      '25 products',
+      'Up to 10,000 subscribers',
+      'Advanced analytics',
+      '24-hour support response time',
+      'Marketing automations',
+    ],
+    paid: true
+  },
+]
+</script>
