5bcab52e8c38c5dc3c6939ba0191eea8d90202d8
Wiktor Morawski <wiktor.morawski@recruitee.com>
2023-08-18 23:58:11 +0200

    feat: add auth site

--- apps/auth/src/main.js
+++ apps/auth/src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import SignInPage from "./pages/SignInPage/SignInPage.vue";
+import SignUpPage from "./pages/SignUpPage/SignUpPage.vue";
 
-createApp(App).mount('#app')
+const history = createWebHistory();
+const routes = [
+    {
+        path: "/",
+        component: SignInPage
+    },
+    {
+        path: "/customers",
+        component: SignUpPage
+    },
+];
+const router = createRouter({
+    history,
+    routes
+});
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
